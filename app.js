5c235a133f0775d90c0b3b74b80f29a9839e79ca
ignacio russo <ignaciorusso0912@gmail.com>
2022-07-11 19:45:58 -0300

    funcionamiento del envio de mail

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require("dotenv").config();
+
 var IndexRouter = require('./routes/index');
 var NosotrosRouter = require ("./routes/nosotros");
 var ServiciosRouter = require("./routes/servicios")
